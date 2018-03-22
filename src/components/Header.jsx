c300971e8de43118369acda7ecec42b99c772079
Damanage <derongar@gmail.com>
2018-03-22 17:55:10 +0300

    code refactoring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-// import ToolBarLink from './ToolBarLink';
+
 import HeaderDesktop from './HeaderDesktop';
 import HeaderMobile from './HeaderMobile';
-// import Logo from './Logo';
+
 
 
 class Header extends React.Component{
