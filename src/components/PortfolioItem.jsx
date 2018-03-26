421f3a5ba4f7f77c593fbdaecd20fabad9bb3938
Damanage <derongar@gmail.com>
2018-03-26 19:14:31 +0300

    portfolio item list

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const PortfolioItem = ({ imgAlt, caption, imgLink, ...props}) => {
     return(
         <figure className="portfolio-item">
-            <img src={imgLink} alt={imgAlt}/>
+            <div className="img-wrp">
+                <img src={imgLink} alt={imgAlt}/>
+            </div>
             <figcaption>
                 {caption}
             </figcaption>
--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,22 +1,22 @@
 const portfolioData = [
     {
         id: 1,
-        url: '/img/duck.png',
+        url: '/img/duck.jpg',
         caption: 'cap one'
     },
     {
         id: 2,
-        url: '/img/duck.png',
+        url: '/img/duck.jpg',
         caption: 'cap two'
     },
     {
         id: 3,
-        url: '/img/duck.png',
+        url: '/img/duck.jpg',
         caption: 'cap three'
     },
     {
         id: 4,
-        url: '/img/duck.png',
+        url: '/img/duck.jpg',
         caption: 'cap four'
     }
 ];
